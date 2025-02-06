a56aea3a7f03985cfa97e8c57b1db5104cdb7eca
Willicious <williciousmedia@gmail.com>
2025-02-06 02:37:38 +0000

    Level Search Bugfix - Multiplied the Progress bar maximum by 2 to ensure that it won't be filled before processing is finished

--- FNeoLemmixLevelSelect.pas
+++ FNeoLemmixLevelSelect.pas
@@ -1406,7 +1406,7 @@ begin
 
   // Initialize progress bar and counter
   pbSearchProgress.Position := 0;
-  pbSearchProgress.Max := tvLevelSelect.Items.Count;
+  pbSearchProgress.Max := tvLevelSelect.Items.Count * 2;
   pbSearchProgress.Visible := True;
 
   Progress := 0;
