c9b762b2430c32f1c92878dc371ff9ed709b33f8
Willicious <williciousmedia@gmail.com>
2026-09-04 03:36:17 +0100

    Added 'Original Lemmings' compilation ->
    + Compiled by Proxima and WillLem
    + Contains all original DMA levels
    + The packs may need to be re-ordered at some point, but this is definitely good enough for now
    + Updated MakeSymLinks.bat to include this compilation, Redux and the Welcome pack

--- MakeSymLinks.bat
+++ MakeSymLinks.bat
@@ -12,5 +12,6 @@ mklink /H bin\bass.dll data\external\bass.dll
 mklink /H bin\NLPackerDefaultData.ini data\external\NLPackerDefaultData.ini
 
 if not exist "bin\levels" mkdir "bin\levels"
-mklink /J "bin\levels\Test Levels" "data\external\levels\Test Levels"
-mklink /J "bin\levels\DMA Lemmings Compilation" "data\external\levels\DMA Lemmings Compilation"
+mklink /J "bin\levels\Lemmings_Redux" "data\external\levels\Lemmings_Redux"
+mklink /J "bin\levels\NeoLemmix_Introduction_Pack" "data\external\levels\NeoLemmix_Introduction_Pack"
+mklink /J "bin\levels\Original_Lemmings" "data\external\levels\Original_Lemmings"
